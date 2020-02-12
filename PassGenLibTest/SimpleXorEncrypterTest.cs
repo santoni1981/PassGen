277e921ba9cdaac1b42dd0acda7e8f5d8f861091
Marco Santoni <marco.santoni@epy.it>
2020-02-12 14:33:10 +0100

    Adds tests for SimpleXorEncrypter.

---
+++ PassGenLibTest/SimpleXorEncrypterTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Santoni1981.PassGenLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGenLibTest
+{
+    [TestClass]
+    public class SimpleXorEncrypterTest
+    {
+        private string Key = "ThisIsTheKeyUsedToEncodeTheString";
+        private string PlainTextMessage = "Hello, World!";
+        private byte[] EncryptedMessage = new byte[] { 47, 13, 5, 6, 4, 64, 77, 57, 0, 2, 29, 22, 82 };
+
+        [TestMethod]
+        public void EncryptPlainText()
+        {
+            var result = SimpleXorEncrypter.Encrypt(this.PlainTextMessage, this.Key);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Length, this.EncryptedMessage.Length);
+
+            for (var ix = 0; ix < this.EncryptedMessage.Length; ++ix)
+            {
+                Assert.AreEqual(result[ix], this.EncryptedMessage[ix]);
+            }
+        }
+
+        [TestMethod]
+        public void DecryptToPlainText()
+        {
+            var result = SimpleXorEncrypter.Decrypt(this.EncryptedMessage, this.Key);
+            Assert.AreEqual(result, this.PlainTextMessage);
+        }
+    }
+}
