9fd23c87ae13122d03c2114207a9b3bb933da815
Marco Santoni <marco.santoni@epy.it>
2020-02-12 14:34:26 +0100

    Removes the unused "using".

--- PassGenLibTest/SimpleXorEncrypterTest.cs
+++ PassGenLibTest/SimpleXorEncrypterTest.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Santoni1981.PassGenLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PassGenLibTest
 {
